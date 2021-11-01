b1f131306d27415d6b4c91f382690805b7e283bf
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-11-01 18:21:29 +0800

    update

--- src/modules/mobile/plugins/icon.js
+++ src/modules/mobile/plugins/icon.js
@@ -1,7 +1,11 @@
 import IconSvg from "@/modules/mobile/components/IconSvg";
 
+const resolve = (dir) => {
+  return path.join(__dirname, dir);
+};
+const path = require("path");
 const requireAll = (requireContext) => requireContext.keys().map(requireContext);
-const req = require.context("@/modules/mobile/assets/icons", false, /\.svg$/);
+const req = require.context(resolve("../assets/icons"), false, /\.svg$/);
 requireAll(req);
 
 export default {
